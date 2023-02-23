7453758dd389fcb1fbe712d49f92ec190ae95253
aleksandrveselov <sanek_ves@mail.ru>
2023-02-23 17:40:59 +0300

    create project structure/создана структура проекта

---
+++ utils/funcs.py
@@ -0,0 +1,14 @@
+def load_json(path):
+    pass
+
+
+def format_requisites(requisites):
+    pass
+
+
+def format_datetime(date_operation):
+    pass
+
+
+def format_operation(operation_data):
+    pass
