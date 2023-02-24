38e3df6fc54d5da14673d4989936b2ce159cc4cd
aleksandrveselov <sanek_ves@mail.ru>
2023-02-24 21:34:28 +0300

    added function format_requisites/реализована функция format_requisites

--- utils/funcs.py
+++ utils/funcs.py
@@ -21,12 +21,30 @@ def load_json(path: str) -> list[dict]:
     return filtered_data
 
 
+def format_requisites(requisites: str) -> str:
+    """Маскирует номер карты или счета в формат XXXX XX** **** XXXX или **ХХХХ соответственно.
+    (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом).
+    Если номер не корректный возвращает соответствующее сообщение
+    :param requisites: реквизиты карты или счета
+    :return: отформатированные реквизиты карты или счета
+    """
+    requisites = requisites.split()  # разделение реквизитов на название карты/счета и номер
+    card_number = requisites[-1]  # Выделение номера из списка
 
+    # если номер состоит не только из цифр, то он некорректен
+    if not card_number.isdigit():
+        return 'Некорректный номер карты/счета'
 
+    # иначе если его длина 16 -> это номер карты, если 20 -> это номер счета. Во всех остальных случаях он некорректен
+    elif len(card_number) == 16:
+        hidden_card_number = f'{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}'
+    elif len(card_number) == 20:
+        hidden_card_number = '**' + card_number[-4:]
+    else:
+        return 'Некорректный номер карты/счета'
+    requisites[-1] = hidden_card_number  # замена номера карты в списке requisites на отформатированный
 
-
-def format_requisites(requisites):
-    pass
+    return ' '.join(requisites)
 
 
 def format_datetime(date_operation):
