800140141485e10866dd2f381c78bfc1536aef5f
aleksandrveselov <sanek_ves@mail.ru>
2023-02-26 22:16:21 +0300

    Написан тест функции load_json

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from utils import funcs
 
-OPERATIONS_PATH = 'operations.json'  # Адрес файла с банковскими операциями
-COUNT_OPERATIONS = 5  # количество операций
-
 
 def main():
-    data = funcs.load_json(OPERATIONS_PATH)
+    OPERATIONS_PATH = 'operations.json'  # Адрес файла с банковскими операциями
+    COUNT_OPERATIONS = 5  # количество операций
+
+    data = funcs.load_json(OPERATIONS_PATH)  # загрузка операций из файла
     if isinstance(data, str):
         print(data)
         exit()
 
-    filtered_data = funcs.filter_operations(data)
+    filtered_data = funcs.filter_operations(data)  # отбор выполненных (EXECUTED) операций
     if isinstance(filtered_data, str):
         print(data)
         exit()
 
-    sorted_data = funcs.sort_operations(filtered_data, COUNT_OPERATIONS)
+    sorted_data = funcs.sort_operations(filtered_data, COUNT_OPERATIONS)  # сортировка операций по дате
     if isinstance(sorted_data, str):
         print(sorted_data)
         exit()
 
+    # Форматирование и вывод на экран информации об операциях
     for operation in sorted_data:
         print(funcs.format_operation(operation))
         print()
--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,8 +1,40 @@
+import json
+
 import pytest
 from utils import funcs
 
-TEST_PATH = 'test_operations.json'
-
 
 def test_load_json():
+    test_path = 'missing_file.json'
+    data = funcs.load_json(test_path)
+    assert data == f'Ошибка при обращении к файлу {test_path}. Проверьте правильность введенного имени'
+
+    test_path = 'tests/test_operations.json'
+    with open(test_path, 'r', encoding='UTF-8') as test_json_file:
+        test_data = json.load(test_json_file)
+    data = funcs.load_json(test_path)
+    assert data == test_data
+
+
+def test_filter_operations():
+    pass
+
+
+def test_sort_operations():
+    pass
+
+
+def test_convert_date():
+    pass
+
+
+def test_format_requisites():
+    pass
+
+
+def test_format_datetime():
+    pass
+
+
+def test_format_operations():
     pass
